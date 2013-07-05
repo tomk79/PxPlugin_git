8b944dbf8324e23670bc7268e5047739b419db5d
Tomoya Koyanagi <tomk79@gmail.com>
2013-07-06 05:56:07 +0900

    git log 表示を整形した。

--- plugins/git/register/pxcommand.php
+++ plugins/git/register/pxcommand.php
@@ -27,25 +27,28 @@ class pxplugin_git_register_pxcommand extends px_bases_pxcommand{
 		$gitHelper = $obj->factory_gitHelper();
 
 		$src = '';
-		$src .= '<p>activility: '.($gitHelper->is_enabled_git_command()?'true':'false').'</p>'."\n";
-
 		$src .= '<h2>git log</h2>';
 		$gitlog = $gitHelper->get_log();
 		$src .= '<p>'.count($gitlog).' commits.</p>'."\n";
 		if(count($gitlog)){
-			$src .= '<dl>'."\n";
+			$src .= '<dl style="max-height:16em; overflow:auto; padding-right:1em;">'."\n";
 			foreach( $gitlog as $gitlog_row ){
-				$src .= '<dt class="large">'.t::h($gitlog_row['subject']).'</dt>'."\n";
+				$src .= '<dt class="large" style="font-weight:bold; margin:1.5em 0 0.5em 0;">'.t::h($gitlog_row['subject']).'</dt>'."\n";
 				$src .= '	<dd class="small">commit: '.t::h($gitlog_row['commit']).'</dd>'."\n";
 				$src .= '	<dd class="small">author: '.t::h($gitlog_row['author']).'</dd>'."\n";
 				$src .= '	<dd class="small">date: '.t::h($gitlog_row['date']).'</dd>'."\n";
-				$src .= '	<dd>'.t::text2html($gitlog_row['description']).'</dd>'."\n";
+				if( strlen(trim($gitlog_row['description'])) ){
+					$src .= '	<dd><div style="padding:0.5em 1em; background-color:#f9f9f9; border:1px solid #aaaaaa;">'.t::text2html($gitlog_row['description']).'</div></dd>'."\n";
+				}
 			}
 			$src .= '</dl>'."\n";
 		}
 
 		$src .= '<h2>git status</h2>';
 		$src .= t::text2html($gitHelper->get_status());
+
+		$src .= '<hr />'."\n";
+		$src .= '<p>git enabled: '.($gitHelper->is_enabled_git_command()?'true':'false').'</p>'."\n";
 
 		print $this->html_template($src);
 		exit;
